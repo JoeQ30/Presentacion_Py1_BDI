f5f41f5f6d9422395a3329c5f9b67d4d2ff53dcb
Joe <joquiros@estudiantec.cr>
2024-04-21 22:48:05 -0600

    Cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { EmpleadosComponent } from './components/empleados/empleados.component';
+import { MovimientosComponent } from './components/movimientos/movimientos.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, LoginComponent, EmpleadosComponent, MovimientosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { EmpleadosComponent } from './components/empleados/empleados.component';
+import { MovimientosComponent } from './components/movimientos/movimientos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'empleados', component: EmpleadosComponent },
+    { path: 'movimientos', component: MovimientosComponent }
+];
